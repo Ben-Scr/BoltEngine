f94fe279dd2a35517a550fa68cd6b86762cff8c9
Ben <benscr.email@gmail.com>
2025-10-13 21:34:07 +0200

    Update

--- Bolt/Engine/main.cpp
+++ Bolt/Engine/main.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
 #include <chrono>
 #include <filesystem>
+#include <iostream>
 
 #include "Core/Application.hpp"
+#include "GameSystem.hpp"
 #include "Scene/Scene.hpp"
 #include "Scene/SceneManager.hpp"
-#include "GameSystem.hpp"
 
 std::filesystem::path getAssetsPath() {
     auto exePath = std::filesystem::current_path();
     return exePath / "Assets";
 }
 
-
 int main() {
     Bolt::SceneDefinition& def = Bolt::SceneManager::RegisterScene("Test");
     def.AddSystem<Bolt::GameSystem>();
+
     Bolt::Application app{};
     app.Run();
     return 0;
