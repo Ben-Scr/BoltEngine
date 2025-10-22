8f544c544800dd8ca9e52aa5aead22cbf66e9cca
Ben <benscr.email@gmail.com>
2025-10-22 18:41:05 +0200

    Update

--- Bolt/Engine/Bolt.hpp
+++ Bolt/Engine/Bolt.hpp
@@ -1,14 +1,14 @@
 #pragma once
-/* INCLUDES */
+/* INCLUDE FILE */
 
-/* COLLECTIONS */
+// COLLECTIONS
 #include "Collections/Vec2.hpp"
 #include "Collections/Color.hpp"
 #include "Collections/Vec4.hpp"
 #include "Collections/Viewport.hpp"
 #include "Collections/Mat2.hpp"
 
-/* COMPONENTS */
+// COMPONENTS
 #include "Components/BoxCollider2D.hpp"
 #include "Components/Rigidbody2D.hpp"
 #include "Components/SpriteRenderer.hpp"
@@ -16,30 +16,30 @@
 #include "Components/ParticleSystem2D.hpp"
 #include "Components/Tags.hpp"
 
-/* CORE */
+// CORE
 #include "Core/Input.hpp"
 #include "Core/KeyCodes.hpp"
 #include "Core/Time.hpp"
 
-/* DEBUG */
+// DEBUG
 #include "Debugging/Logger.hpp"
 
-/* GRAPHICS */
+// GRAPHICS
 #include "Graphics/Texture2D.hpp"
 #include "Graphics/TextureHandle.hpp"
 #include "Graphics/TextureManager.hpp"
 #include "Graphics/Gizmos.hpp"
 #include "Graphics/Camera2D.hpp"
 
-/* MATH */
+// MATH
 #include "Math/Common.hpp"
 
-/* PHYSICS */
+// PHYSICS
 #include "Physics/Collision2D.hpp"
 #include "Physics/PhysicsUtility.hpp"
 #include "Physics/Box2DWorld.hpp"
 
-/* SCENE */
+// SCENE
 #include "Scene/Scene.hpp"
 #include "Scene/SceneManager.hpp"
 #include "Scene/Entity.hpp"
