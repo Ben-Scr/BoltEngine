15ac61b1c29d3e8c31bb05b60a199d43c34f9970
Ben <benscr.email@gmail.com>
2025-10-26 14:29:35 +0100

    Update

--- Bolt/Engine/Graphics/Renderer.cpp
+++ Bolt/Engine/Graphics/Renderer.cpp
@@ -36,7 +36,7 @@ namespace Bolt {
             return;
         }
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_DEPTH_BUFFER_BIT);
         RenderScenes();
     }
 
