43388262b234e37d3cf193d43d218f6bc96dc465
rrainix <benscr.email@gmail.com>
2025-11-18 13:39:43 +0100

    Update

--- Bolt/Engine/GameSystem.cpp
+++ Bolt/Engine/GameSystem.cpp
@@ -41,7 +41,7 @@ namespace Bolt {
 
 		AudioHandle handle = AudioManager::LoadAudio("Assets/Audio/Sfx/camera-flash.mp3");
 		AudioSource source{ handle };
-		source.SetVolume(0.1f);
+		source.SetVolume(0.1f); 
 		source.Play(); 
 
 		CreatePhysicsEntity<Tag>(scene, Transform2D(Vec2(0, -3), Vec2(100, 1)), BodyType::Static);
