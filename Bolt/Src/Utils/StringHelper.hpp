7054c1ca9b26f500748fb72b71c3d825b5ee3129
rrainix <benscr.email@gmail.com>
2026-02-23 13:37:30 +0100

    Update

--- Bolt/Src/Utils/StringHelper.hpp
+++ Bolt/Src/Utils/StringHelper.hpp
@@ -131,8 +131,4 @@ namespace Bolt {
 		//	return !str.empty() && std::all_of(str.begin(), str.end(), std::isalpha);
 		//}
 	};
-
-	inline std::ostream& operator<<(std::ostream& os, bool b) {
-		return os << (b ? "true" : "false");
-	}
 }
