3abc138f79c4e56e130b3b156af7f2f7110078b2
Ben Schneider <benscr.email@gmail.com>
2026-03-24 18:52:08 +0100

    Update

--- Sandbox/Src/Sandbox.cpp
+++ Sandbox/Src/Sandbox.cpp
@@ -6,6 +6,7 @@
 
 #include "Systems/ImGuiDebugSystem.hpp"
 #include "GameSystem.hpp"
+#include <Scene/EntityHelper.hpp>
 
 #include <iostream>
 
@@ -13,7 +14,10 @@
 #include <BoltPhys/Collider.hpp>
 #include <BoltPhys/PhysicsWorld.hpp>
 #include <BoltPhys/StaticBody.hpp>
+#include <BoltPhys/DynamicBody.hpp>
 #include <BoltPhys/BoxCollider.hpp>
+
+#include <Core/Time.hpp>
 
 using namespace Bolt;
 using namespace BoltPhys;
@@ -22,6 +26,10 @@ using namespace BoltPhys;
 class Sandbox : public Bolt::Application {
 public:
 	PhysicsWorld world;
+	BoltPhys::DynamicBody player;
+	Entity playerEntity{ Entity::Null };
+
+	BoltPhys::StaticBody obstacle;
 
 	void ConfigureScenes() override {
 		Bolt::SceneDefinition& def = GetSceneManager()->RegisterScene("Game");
@@ -33,15 +41,30 @@ public:
 	~Sandbox() override = default;
 
 	void Start() override {
-		BoltPhys::DynamicBody player;
+		Entity obstacleEnt = Bolt::EntityHelper::CreateWith<SpriteRendererComponent, Transform2DComponent>();
+
+		playerEntity = Bolt::EntityHelper::CreateWith<SpriteRendererComponent, Transform2DComponent>();
+
 		BoltPhys::BoxCollider playerCollider({ 0.5f, 1.0f });
+		BoltPhys::BoxCollider obstacleCollider({ 10.f, 1.0f });
+
+		obstacle.SetPosition(BoltPhys::Vec2(0, -5.0f));
+
+		world.RegisterBody(obstacle);
+		world.RegisterCollider(obstacleCollider);
+		world.AttachCollider(obstacle, obstacleCollider);
+
+
+		obstacleEnt.GetComponent<Transform2DComponent>().Position = Bolt::Vec2(0, -5.0f);
 
 		world.RegisterBody(player);
 		world.RegisterCollider(playerCollider);
 		world.AttachCollider(player, playerCollider);
 	}
 	void Update() override {
-		world.Step(1.0f / 60.0f);
+		world.Step(Bolt::Time::GetFixedDeltaTime());
+		auto position = player.GetPosition();
+		playerEntity.GetComponent<Transform2DComponent>().Position = Bolt::Vec2(position.x, position.y);
 	}
 	void OnPaused() override {
 
