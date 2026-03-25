ba5e33bde7d44f8a23490d9c9f969e49d19fa3dc
Ben Schneider <benscr.email@gmail.com>
2026-03-25 20:40:32 +0100

    Update

--- Sandbox/Src/Sandbox.cpp
+++ Sandbox/Src/Sandbox.cpp
@@ -1,27 +1,11 @@
 #include <Bolt.hpp>
 #include "Scene/SceneDefinition.hpp"
 #include "Scene/SceneManager.hpp"
-#include "Scene/Scene.hpp"
-#include "Components/Components.hpp"
 
 #include "Systems/ImGuiDebugSystem.hpp"
 #include <Scene/EntityHelper.hpp>
 
-#include <iostream>
-
-#include <BoltPhys/Body.hpp>
-#include <BoltPhys/BodyType.hpp>
-#include <BoltPhys/Collider.hpp>
-#include <BoltPhys/PhysicsWorld.hpp>
-#include <BoltPhys/BoxCollider.hpp>
-
-#include <Graphics/Gizmos.hpp>
-
-#include <Core/Time.hpp>
-#include <Core/Input.hpp>
-
 using namespace Bolt;
-using namespace BoltPhys;
 
 
 class Sandbox : public Bolt::Application {
